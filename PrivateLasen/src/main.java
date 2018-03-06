a6db3e5e16e68f3501070464817da5f4829f76d7
rimonp <rimonp3@gmail.com>
2018-03-06 20:14:16 +0200

    first commit from eclipse

---
+++ PrivateLasen/src/main.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+
+
+		//		//כתוב תוכנית שמגדירה 3 מספרים עשרונים ומדפיסה את הסכום של שלושתם
+		//		double num= 5.9;
+		//		double num1=-0.03;
+		//		double num2=1200.2;
+		//		
+		//		double sum = num+num1+num2;//סכום המספרים
+		//		System.out.println(sum);
+		//		double avg=sum/3;//ממוצע המספרים
+		//		System.out.println(avg);
+		//		
+		//		num+=5;//num=num+5;להוסיף לו 5
+		//		 
+		//		num2+=1;//num2=num2+1 || num2++   להוסיף 1
+		//		
+		//		int num4 =1;
+		//		System.out.println(num4);
+		//		num4++;
+		//		System.out.println(num4);
+		//		num4++;
+		//		System.out.println(num4);
+		//		num4++;
+		//		System.out.println(num4);
+		//		num4++;
+		//		System.out.println(num4);
+
+		//		while(num4 <= 1000) { //מדפיס מ1 עד 1000
+		//			System.out.println(num4);
+		//			num4++;
+		//		}
+		//		
+
+		
+		//		//תקלוט מספר מהמשתמש והתוכנית תדפיס האם הוא זוגי או אי זוגי
+		//		Scanner input = new Scanner(System.in);
+		//		if(num%2==0) {
+		//			System.out.println("even");
+		//		}
+		//		if(num%2!=0) {
+		//			System.out.println("odd");
+		//		}
+
+		
+		//כתוב תוכנית שמדפיסה על המסך את כל האיזוגים מ1עד 1000	
+
+//		int num = 1;
+//		while (num<=1000) {
+//			if(num%2!=0) {
+//				System.out.println(num);
+//			}
+//			num++;
+//
+//		}
+//
+
+		
+		//int num = 1;
+//		while (num<=100) {
+//			if(num==7) {
+//				System.out.println("boom");
+//			}
+//			else if (num%7==0) { //בודק כפולה של 7
+//				System.out.println("boom");
+//			
+//			}
+//			else if (num%10==7) {//בודק האם ספרת האחדות היא 7
+//				System.out.println("boom");
+//			}
+//			else if (num/10==7) {// בודק האם ספרת העשרות היא 7
+//				System.out.println("boom");
+//			}
+//			else {// אם הוא לא עמד באף תנאי אז תדפיס את המספר עצמו
+//			System.out.println(num);
+//			}
+//			num++;// חובה לקדם את המספר אחרת הלולאה תמשיך עד אינסוף
+//		}
+
+
+////	for(int num=1;num<=100;num++) {
+//			
+//			if(num==7||num%7==0||num%10==7||num/10==7) {
+//				System.out.println("BOOM");
+//			}
+//			else {
+//				System.out.println(num);
+		
+	}
+}
+
+
+
